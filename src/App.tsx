ea6aed3a82dc1ea493e3ef96293e548b4c09827c
GiftShower <hj.samuel0516@gmail.com>
2021-08-24 18:51:50 +0900

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {MouseEventHandler, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from "./Modal/Modal";
+import {Simulate} from "react-dom/test-utils";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [isShowing, setIsShowing] = useState(false);
+    const openModal = () => {
+        setIsShowing(true);
+        copyToClipboard();
+    };
+    useEffect(() =>  {
+        if(isShowing) {
+            const notiTimer = setTimeout(() => {
+                setIsShowing(false);
+            }, 3000);
+            return () => clearTimeout(notiTimer);
+        }
+    }, [isShowing]);
+
+    return (
+        <div className={"App"}>
+            <h1>멀티그라운드에 오신 것을 환영합니다!</h1>
+            <h2>Welcome to MultiGround!</h2>
+            <p/>
+            <p>서버 주소: giftshower.games</p>
+            <p>서버 버전: 1.17.1-MultiGround</p>
+            <div>
+                <button className={"btn"}>런처 다운로드하기</button>
+                &nbsp;
+                <button className={"btn"} onClick={openModal}>주소 복사하기</button>
+            </div>
+            <div>{isShowing && <Modal message="서버 주소 복사 완료!" />}</div>
+            <div className={"navbar"}>
+                <a href="">Home</a>
+                <a href="https://github.com/MultiGround">GitHub</a>
+            </div>
+        </div>
+    );
+}
+
+const copyToClipboard = () =>{
+    navigator.clipboard.writeText("giftshower.games");
 }
 
 export default App;
